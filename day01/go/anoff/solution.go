07f01e0b0c5d775deba00b0b6f6f03a1f7d29262
anoff <offenhaeuser@gmail.com>
2018-12-01 12:34:03 +0100

    :star2: feat: solution for part 2

--- day01/go/anoff/solution.go
+++ day01/go/anoff/solution.go
@@ -4,9 +4,10 @@ import (
     "io/ioutil"
     "strings"
     "strconv"
+    "math"
 )
 func main() {
-    byteContent, err := ioutil.ReadFile("input.txt")
+    byteContent, err := ioutil.ReadFile("./input.txt")
     if err != nil {
         fmt.Print(err)
     }
@@ -14,13 +15,50 @@ func main() {
     stringContent := string(byteContent)
     lines := strings.Split(stringContent, "\n")
     
-    sum := 0
+    // Part 1
+    currentFrequency := 0
     for _, v := range lines {
         if len(v) == 0 {
             continue
         }
         num, _ := strconv.Atoi(v)
-        sum += num
+        currentFrequency += num
     }
-    fmt.Println(sum)
+    fmt.Printf("Solution for part 1: %d\n", currentFrequency)
+
+    // Part 2
+    var frequencies []int
+    currentFrequency = 0
+    frequencies = append(frequencies, currentFrequency)
+    for i := 0; i < 1000; i++ { // loop multiple times over the list
+        for _, v := range lines {
+            if len(v) == 0 {
+                continue
+            }
+            num, _ := strconv.Atoi(v)
+            currentFrequency += num
+            
+            isMatch, _ := hasElement(frequencies, currentFrequency)
+            if isMatch {
+                fmt.Printf("Solution for part 2: %d, found in iteration %d\n", currentFrequency, i)
+                return
+            }
+            frequencies = append(frequencies, currentFrequency)
+            // fmt.Printf("number of frequencies: %d, currentFreq: %d, minDiff: %d\n", len(frequencies), currentFrequency, diff)
+        }
+    }
+}
+
+func hasElement (arr []int, elm int) (bool, int) {
+    minDiff := math.Inf(0)
+    for _, n := range arr {
+        diff := math.Abs(float64(n - elm))
+        if diff < minDiff {
+            minDiff = diff
+        }
+        if n == elm {
+            return true, int(minDiff)
+        }
+    }
+    return false, int(minDiff)
 }
