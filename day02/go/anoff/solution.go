f8b63e863ee8700188b8e2f449e527f554591f59
anoff <offenhaeuser@gmail.com>
2018-12-02 10:02:01 +0100

    :star2: feat: second challenge for day2

--- day02/go/anoff/solution.go
+++ day02/go/anoff/solution.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	reader := getPipeReader()
 
+	// init part 1
 	doubles := 0
 	triples := 0
 
+	// init part 2
 	var previousLines []string
 
 	for {
@@ -33,7 +35,8 @@ func main() {
 		for _, line2 := range previousLines {
 			matchDetected := checkAlmostIdentical(lineString, line2)
 			if matchDetected {
-				fmt.Printf("Solution for part2: YAY\n")
+				chars := overlappingChars(lineString, line2)
+				fmt.Printf("Solution for part2: %s, (l1: %s, l2: %s)\n", string(chars), lineString, line2)
 			}
 		}
 		previousLines = append(previousLines, lineString)
@@ -64,7 +67,27 @@ func checkOccurences(str string) (int, int) {
 	return doubleCount, tripleCount
 }
 
-// 
+// check if only 1 character (position aware) is mismatched
 func checkAlmostIdentical(line1 string, line2 string) bool {
-	return false
+	mismatches := 0
+	for i := 0; i < len(line1); i++ {
+		if line1[i] != line2[i] {
+			mismatches++
+			if (mismatches > 1) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// identify overlapping characters from two strings
+func overlappingChars(line1 string, line2 string) []byte {
+	var chars []byte
+	for i := 0; i < len(line1); i++ {
+		if line1[i] == line2[i] {
+			chars = append(chars, line1[i])
+		}
+	}
+	return chars
 }
