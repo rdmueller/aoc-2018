fe80afe550e00961827752abb42b5634e839e45e
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-03 07:09:21 +0100

    Day03 part 1 improved solution by tracking actual claims.

--- day03/java/highsource/src/main/java/SolutionPart1.java
+++ day03/java/highsource/src/main/java/SolutionPart1.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SolutionPart1 {
 
@@ -13,17 +14,16 @@ public class SolutionPart1 {
 		try (BufferedReader reader = new BufferedReader(
 				new InputStreamReader(SolutionPart1.class.getResourceAsStream("input.txt")))) {
 
-			final Map<SquareInch, AtomicInteger> fabric = new TreeMap<>(
+			final Map<SquareInch, Set<Claim>> fabric = new TreeMap<>(
 					Comparator.<SquareInch>comparingInt(o -> o.x).thenComparing(o -> o.y));
 
 			for (String line; (line = reader.readLine()) != null;) {
 				Claim claim = Claim.parse(line);
-				claim.area().stream()
-						.forEach(xy -> fabric.computeIfAbsent(xy, ignored -> new AtomicInteger(0)).incrementAndGet());
+				claim.area().stream().forEach(xy -> fabric.computeIfAbsent(xy, ignored -> new HashSet<>()).add(claim));
 			}
 
 			final long numberOfSquareInchesOfFabricWithinTwoOrMoreClaims = fabric.entrySet().stream()
-					.filter(entry -> entry.getValue().get() >= 2).count();
+					.filter(entry -> entry.getValue().size() >= 2).count();
 			System.out.println("Number of square inches of fabric within two or more claims:"
 					+ numberOfSquareInchesOfFabricWithinTwoOrMoreClaims);
 		}
