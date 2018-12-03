6c44bf30de43b6110a8f180f08eadcfae3ac7b56
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-03 06:14:47 +0100

    Day03 part 1 setup.

---
+++ day03/java/highsource/src/main/java/SolutionPart1.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SolutionPart1 {
+
+	public static void main(String[] args) throws IOException {
+
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(SolutionPart1.class.getResourceAsStream("input.txt")))) {
+
+			for (String line; (line = reader.readLine()) != null;) {
+				System.out.println(line);
+			}
+		}
+	}
+}
