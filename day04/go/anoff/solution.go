369538e07882e062a2fbcd92f43a8ad91692c29c
anoff <offenhaeuser@gmail.com>
2018-12-04 20:08:32 +0100

    :star2: feat: solution+docs part2

--- day04/go/anoff/solution.go
+++ day04/go/anoff/solution.go
@@ -18,47 +18,12 @@ func main() {
 	input := getInput(true)
 	
 	// fmt.Println(strings.Join(input, "\n"))
-	strategy1(input)
+	shifts := generateShifts(input)
+	strategy1(shifts)
+	strategy2(shifts)
 }
 
-func strategy1(log []string /* sorted input */) {
-	var shifts []Shift
-	activeShift := Shift{guard: -1}
-	fellAsleep := -1
-	for _, entry := range log {
-		guardId := detectShiftChange(entry)
-		date := extractDate(entry)
-		if guardId > -1 {
-			// check if this is NOT the first shift
-			if activeShift.guard > -1 {
-				shifts = append(shifts, activeShift)
-			}
-			// fmt.Printf("Guard change detected on %s from %d to %d\n", date, activeShift.guard, guardId)
-			activeShift.guard = guardId
-			activeShift.asleep = []int{}
-			fellAsleep = -1
-		}
-		activeShift.date = date
-
-		if strings.Contains(entry, "falls asleep") {
-			fellAsleep = extractMinute(entry)
-		}
-
-		if strings.Contains(entry, "wakes up") {
-			if fellAsleep > -1 {
-				wakeupMin := extractMinute(entry)
-				for i := fellAsleep; i < wakeupMin; i++ {
-					activeShift.asleep = append(activeShift.asleep, i)
-				}
-			} else {
-				fmt.Printf("🤯 Guard %d woke up withouth falling asleep :ooo\n", activeShift.guard)
-			}
-		}
-	}
-	// make sure last shift is also stored (no shift change in file)
-	shifts = append(shifts, activeShift)
-
-	// fmt.Println(shifts)
+func strategy1(shifts []Shift) {
 
 	// find longest sleeping guard
 	guardsSleepDuration := make(map[int]int)
@@ -103,6 +68,88 @@ func strategy1(log []string /* sorted input */) {
 	fmt.Printf("Solution for part1: %d \nat minute %d guard#%d is asleep the most often (%d shifts)\n", maxShiftsAsleep * longestSleepingGuard, maxShiftsAsleep, longestSleepingGuard, sleepMap[maxShiftsAsleep])
 }
 
+func strategy2(shifts []Shift) {
+	// create a map of guard=[60]times asleep for given minute
+	sleepMap := make(map[int][]int) // key=guard, val=[sleepCount-min0, sleepCount-min1, ..-59]
+	for _, shift := range shifts {
+		if _, exists := sleepMap[shift.guard]; !exists {
+			for j := 0; j < 60; j ++ {
+				sleepMap[shift.guard] = append(sleepMap[shift.guard], 0)
+			}
+		}
+		for _, asleepMin := range shift.asleep {
+			sleepMap[shift.guard][asleepMin]++
+		}
+	}
+	// fmt.Println(sleepMap)
+
+	// loop over 60 mins and see which guard has the highest sleepcount in comparison to others
+	sleepyGuard := 0
+	sleepyCount := 0 // how many more times the guard slept
+	sleepyMinute := 0 // minute at which the record was achieved
+
+	for min := 0; min < 60; min ++ {
+		maxCount := 0 // max count seen for this given minute over all guards
+		maxGuard := 0 // guard for which max count was seen
+		for guard, counts := range sleepMap {
+			if counts[min] > maxCount {
+				maxGuard = guard
+				maxCount = counts[min]
+			}
+		}
+		// if this minute poses a new overall high update the vars
+		if maxCount > sleepyCount {
+			sleepyGuard = maxGuard
+			sleepyMinute = min
+			sleepyCount = maxCount
+		}
+	}
+
+	fmt.Printf("Solution for part 1: %d\nseen for guard #%d at min:%d with %d sleeps\n", sleepyGuard*sleepyMinute, sleepyGuard, sleepyMinute, sleepyCount)
+}
+
+// tag::generateShifts[]
+func generateShifts(log []string) []Shift {
+	var shifts []Shift
+	activeShift := Shift{guard: -1}
+	fellAsleep := -1
+	for _, entry := range log {
+		guardId := detectShiftChange(entry)
+		date := extractDate(entry)
+		if guardId > -1 {
+			// check if this is NOT the first shift
+			if activeShift.guard > -1 {
+				shifts = append(shifts, activeShift)
+			}
+			// fmt.Printf("Guard change detected on %s from %d to %d\n", date, activeShift.guard, guardId)
+			activeShift.guard = guardId
+			activeShift.asleep = []int{}
+			fellAsleep = -1
+		}
+		activeShift.date = date
+
+		if strings.Contains(entry, "falls asleep") {
+			fellAsleep = extractMinute(entry)
+		}
+
+		if strings.Contains(entry, "wakes up") {
+			if fellAsleep > -1 {
+				wakeupMin := extractMinute(entry)
+				for i := fellAsleep; i < wakeupMin; i++ {
+					activeShift.asleep = append(activeShift.asleep, i)
+				}
+			} else {
+				fmt.Printf("🤯 Guard %d woke up withouth falling asleep :ooo\n", activeShift.guard)
+			}
+		}
+	}
+	// make sure last shift is also stored (no shift change in file)
+	shifts = append(shifts, activeShift)
+
+	return shifts
+}
+// end::generateShifts[]
+
 // extract the guard id from a new shift entry
 // returns -1 if no shift change detected
 func detectShiftChange(entry string) int {
@@ -135,3 +182,4 @@ func sumSleepTimes(shifts []Shift, guardId int) int {
 	}
 	return counter
 }
+
