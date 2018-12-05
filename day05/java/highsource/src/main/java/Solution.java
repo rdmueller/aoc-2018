c06054b6f3d0d230855975c160819b3c3c3e219d
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-05 06:23:42 +0100

    Day05 setup.

---
+++ day05/java/highsource/src/main/java/Solution.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(Solution.class.getResourceAsStream("input.txt")))) {
+
+			for (String line; (line = reader.readLine()) != null;) {
+				System.out.println(line);
+				System.out.println(line.length());
+			}
+		}
+	}
+}
