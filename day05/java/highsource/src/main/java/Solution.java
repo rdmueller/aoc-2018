48b24ce3822735b33a972b4b258b8bd39ffb7fe2
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-05 06:50:35 +0100

    Day05 solved part 1.

--- day05/java/highsource/src/main/java/Solution.java
+++ day05/java/highsource/src/main/java/Solution.java
@@ -3,16 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
-
+	
 	public static void main(String[] args) throws IOException {
 
 		try (BufferedReader reader = new BufferedReader(
 				new InputStreamReader(Solution.class.getResourceAsStream("input.txt")))) {
 
 			for (String line; (line = reader.readLine()) != null;) {
-				System.out.println(line);
-				System.out.println(line.length());
+				solvePart1(line);
 			}
 		}
+	}
+
+	private static String solvePart1(String str) {
+		final StringBuffer stringBuffer = new StringBuffer(str);
+
+		boolean somethingWasRemoved = false;
+		do {
+			somethingWasRemoved = false;
+			for (int index = 0; index < stringBuffer.length() - 1; index++) {
+				char currentChar = stringBuffer.charAt(index);
+				char nextChar = stringBuffer.charAt(index + 1);
+				if (Character.isUpperCase(currentChar) && (Character.toLowerCase(currentChar) == nextChar) ||
+						Character.isLowerCase(currentChar) && (Character.toUpperCase(currentChar) == nextChar)) {
+					stringBuffer.delete(index, index + 2);
+					somethingWasRemoved = true;
+				}
+			}
+		} while (somethingWasRemoved);
+		System.out.println("Result:" + stringBuffer.length());
+		return stringBuffer.toString();
 	}
 }
