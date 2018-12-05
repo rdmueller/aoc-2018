4bcab6c4c43bedfe6c8d90149e021f781b48e87d
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-05 07:32:10 +0100

    Day05 solved part 2. Added documentation.

--- day05/java/highsource/src/main/java/Solution.java
+++ day05/java/highsource/src/main/java/Solution.java
@@ -1,37 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 
 public class Solution {
-	
+
 	public static void main(String[] args) throws IOException {
 
 		try (BufferedReader reader = new BufferedReader(
 				new InputStreamReader(Solution.class.getResourceAsStream("input.txt")))) {
-
 			for (String line; (line = reader.readLine()) != null;) {
-				solvePart1(line);
+				System.out.println("Result part 1: " + solvePart1(line).length());
+				System.out.println("Result part 2: " + solvePart2(line).length());
 			}
 		}
 	}
 
 	private static String solvePart1(String str) {
-		final StringBuffer stringBuffer = new StringBuffer(str);
+		return fullyReact(new StringBuffer(str)).toString();
+	}
 
-		boolean somethingWasRemoved = false;
+	private static StringBuffer fullyReact(final StringBuffer stringBuffer) {
 		do {
-			somethingWasRemoved = false;
-			for (int index = 0; index < stringBuffer.length() - 1; index++) {
-				char currentChar = stringBuffer.charAt(index);
-				char nextChar = stringBuffer.charAt(index + 1);
-				if (Character.isUpperCase(currentChar) && (Character.toLowerCase(currentChar) == nextChar) ||
-						Character.isLowerCase(currentChar) && (Character.toUpperCase(currentChar) == nextChar)) {
-					stringBuffer.delete(index, index + 2);
-					somethingWasRemoved = true;
-				}
+		} while (react(stringBuffer));
+		return stringBuffer;
+	}
+
+	private static boolean react(final StringBuffer stringBuffer) {
+		boolean somethingWasRemoved;
+		somethingWasRemoved = false;
+		for (int index = 0; index < stringBuffer.length() - 1; index++) {
+			char currentChar = stringBuffer.charAt(index);
+			char nextChar = stringBuffer.charAt(index + 1);
+			if (Character.isUpperCase(currentChar) && (Character.toLowerCase(currentChar) == nextChar)
+					|| Character.isLowerCase(currentChar) && (Character.toUpperCase(currentChar) == nextChar)) {
+				stringBuffer.delete(index, index + 2);
+				somethingWasRemoved = true;
 			}
-		} while (somethingWasRemoved);
-		System.out.println("Result:" + stringBuffer.length());
-		return stringBuffer.toString();
+		}
+		return somethingWasRemoved;
+	}
+
+	private static String solvePart2(String str) {
+		return
+
+		str.chars().mapToObj(c -> (char) Character.toLowerCase(c)).distinct().
+		//
+				map(c -> removeUnitType(new StringBuffer(str), c)).
+				//
+				map(Solution::fullyReact).
+				//
+				min(Comparator.comparingInt(StringBuffer::length)).
+				//
+				map(Object::toString).
+				//
+				orElseThrow(IllegalArgumentException::new);
+	}
+
+	private static StringBuffer removeUnitType(StringBuffer stringBuffer, char c) {
+		for (int index = 0; index < stringBuffer.length() - 1; index++) {
+			char currentChar = stringBuffer.charAt(index);
+			if (Character.toLowerCase(currentChar) == Character.toLowerCase(c)) {
+				stringBuffer.delete(index, index + 1);
+				index--;
+			}
+		}
+		return stringBuffer;
 	}
 }
