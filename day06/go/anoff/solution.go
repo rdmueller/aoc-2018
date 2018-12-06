e186a0b7b986cedcafa957dd785b38d8680c2edf
anoff <offenhaeuser@gmail.com>
2018-12-06 08:01:29 +0100

    :star2: feat: build grid

--- day06/go/anoff/solution.go
+++ day06/go/anoff/solution.go
@@ -7,23 +7,50 @@ import (
 )
 
 type Coord struct {
-	x uint16
-	y uint16
+	id int
+	x int
+	y int
 }
 
 func main() {
 	input := getInput(false)
 	
 	var coords []Coord
-	for _, line := range input {
-		coords = append(coords, extractCoords(line))
+	var gridXmax int // helper to find max val
+	var gridYmax int
+	for ix, line := range input {
+		coord := extractCoords(line)
+		coord.id = ix+1 // use line# as id (make sure 0 remains empty for special case)
+		coords = append(coords, coord)
+		if coord.x > gridXmax {
+			gridXmax = coord.x
+		}
+		if coord.y > gridYmax {
+			gridYmax = coord.y
+		}
 	}
 	fmt.Println(coords)
 	//fmt.Printf("Solution for part 1: %d, (%s)\n", len(imploded), imploded)
 
-	// var grid [500][500]uint16
-	// gridXmax := 0 // helper to find max val
-	// gridYmax := 0
+	var grid [10][10]uint8
+	for x := 0; x <= gridXmax; x++ {
+		for y := 0; y <= gridYmax; y++ {
+			shortestDistance := 500
+			closestNeighbor := 0
+			for _, coord := range coords {
+				dist := manhattenDistance(coord.x, coord.y, x, y)
+				if dist < shortestDistance {
+					shortestDistance = dist
+					closestNeighbor = coord.id
+				} else if (dist == shortestDistance) {
+					closestNeighbor = 0 // 0 represents a tie (multiple closest neighbors)
+				}
+			}
+			grid[x][y] = uint8(closestNeighbor)
+		}
+	}
+
+	fmt.Println(grid)
 }
 
 func extractCoords(input string) Coord {
@@ -31,12 +58,12 @@ func extractCoords(input string) Coord {
 	x, _ := strconv.Atoi(parts[0])
 	y, _ := strconv.Atoi(parts[1])
 	
-	coord := Coord{uint16(x), uint16(y)}
+	coord := Coord{x: x, y: y}
 	return coord
 }
 
-func manhattenDistance(x1 uint16, y1 uint16, x2 uint16, y2 uint16) uint16 {
-	var diff uint16
+func manhattenDistance(x1 int, y1 int, x2 int, y2 int) int {
+	var diff int
 	if x1 < x2 {
 		diff += x2-x1
 	} else {
