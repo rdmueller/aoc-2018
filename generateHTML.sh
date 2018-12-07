b140aaaabb9a651cfdabd81acd24b0b5406f99bf
Ralf D. Müller <ralf.d.mueller@gmail.com>
2018-12-07 20:38:09 +0000

    fix #37

--- generateHTML.sh
+++ generateHTML.sh
@@ -1,5 +1,6 @@
 ./gradlew clean
 cd docs/groovy
+wget https://bintray.com/artifact/download/groovy/maven/apache-groovy-binary-2.5.4.zip
 unzip apache-groovy-binary-2.5.4.zip
 cd ..
 ./generateIndex.groovy
