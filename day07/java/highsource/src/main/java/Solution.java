03b9810f193b5c22d0ee84c9e7f9500251261a41
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-07 07:15:26 +0100

    Day 07.1 solved.

--- day07/java/highsource/src/main/java/Solution.java
+++ day07/java/highsource/src/main/java/Solution.java
@@ -1,8 +1,15 @@
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toMap;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
@@ -18,7 +25,7 @@ public class Solution {
 		final Map<Character, Collection<Character>> outgoingEdges = new TreeMap<>();
 
 		try (BufferedReader reader = new BufferedReader(
-				new InputStreamReader(Solution.class.getResourceAsStream("test1.txt")))) {
+				new InputStreamReader(Solution.class.getResourceAsStream("input.txt")))) {
 			for (String line; (line = reader.readLine()) != null;) {
 				Matcher matcher = PATTERN.matcher(line);
 				if (!matcher.matches()) {
@@ -26,14 +33,16 @@ public class Solution {
 				} else {
 					Character from = matcher.group(1).charAt(0);
 					Character to = matcher.group(2).charAt(0);
-					System.out.println(from + "->" + to);
 					outgoingEdges.computeIfAbsent(from, c -> new TreeSet<>()).add(to);
 				}
 			}
 		}
-		System.out.println(outgoingEdges);
 
-		final Map<Character, Collection<Character>> incomingEdges = new TreeMap<>();
+		final Map<Character, Collection<Character>> incomingEdges = outgoingEdges.keySet().stream()
+				.collect(toMap(identity(), c -> new TreeSet<>(), (u, v) -> {
+					throw new IllegalStateException(String.format("Duplicate key %s", u));
+				}, TreeMap::new));
+
 		outgoingEdges.entrySet().stream().forEach(entry -> {
 
 			final Character from = entry.getKey();
@@ -41,6 +50,23 @@ public class Solution {
 				incomingEdges.computeIfAbsent(to, c -> new TreeSet<>()).add(from);
 			});
 		});
-		System.out.println(incomingEdges);
+
+		// Part 1
+		{
+
+			Set<Character> finishedSteps = new LinkedHashSet<>();
+			while (finishedSteps.size() != incomingEdges.size()) {
+
+				Character nextStep = incomingEdges.entrySet().stream()
+						// Finished steps does not contain "to"
+						.filter(toFrom -> !finishedSteps.contains(toFrom.getKey()))
+						// Finished steps do contain all "from"
+						.filter(toFrom -> finishedSteps.containsAll(toFrom.getValue()))
+
+						.min(Entry.comparingByKey()).orElseThrow(IllegalStateException::new).getKey();
+				finishedSteps.add(nextStep);
+			}
+			System.out.println("Order of steps: " + finishedSteps.stream().map(Object::toString).collect(joining()));
+		}
 	}
 }
