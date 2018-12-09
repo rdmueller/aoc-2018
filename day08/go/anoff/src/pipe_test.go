0f36fe4f0d2c4994b40a6917b7b5438b195890b2
anoff <offenhaeuser@gmail.com>
2018-12-09 11:18:35 +0100

    :star2: feat: try using state machines while parsing

--- day08/go/anoff/src/pipe_test.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"testing"
-	"github.com/stretchr/testify/assert"
-)
-
-func TestGetInput(t *testing.T) {
-	assert.Equal(t, 1, len(AggregateInputStream()))
-}
--- day08/go/anoff/src/solution.go
+++ day08/go/anoff/src/solution.go
@@ -28,7 +28,35 @@ func parseNodes(def []int) {
 	var nodes []Node
 	var stack []int // queue that points to nodes that are still being progressed (stack)
 
-	for i := 0; i < len(def); i++ {
-
+	state := 0 // 0=new node (header:childCount), 1=header:metaCount, 2=metaEntry
+	for i, val := range def {
+		switch state {
+		case 0:
+			nodes = append(nodes, Node{id: len(nodes), childCount: val})
+			stack = append(stack, len(nodes)-1)
+			parentNode := stack[len(stack)-2]
+			nodes[parentNode].children = append(nodes[parentNode].children, len(nodes)-1)
+			state = 1
+		case 1:
+			activeNode := stack[len(stack)-1]
+			nodes[activeNode].metaCount = val
+			if nodes[activeNode].childCount > 0 {
+				state = 0
+			} else {
+				state = 2
+			}
+		case 2:
+			activeNode := stack[len(stack)-1]
+			nodes[activeNode].meta = append(nodes[activeNode].meta, val)
+			if len(nodes[activeNode].meta) == nodes[activeNode].metaCount {
+				// all meta values read; pop node from open stack
+				stack = stack[:len(stack)-1]
+				nextNode := stack[len(stack)-1]
+				if nodes[nextNode].childCount == len(nodes[nextNode].children) {
+					// all children handled
+					
+				}
+			} else 
+		}
 	}
 }
