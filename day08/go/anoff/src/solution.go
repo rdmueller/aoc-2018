7a76bf83dd904776c1059de45e4ad7d791f29817
anoff <offenhaeuser@gmail.com>
2018-12-09 11:45:50 +0100

    :star2: feat: solution part1 with recursion

--- day08/go/anoff/src/solution.go
+++ day08/go/anoff/src/solution.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"helpers"
+	"math/rand"
 	"strings"
+	"helpers"
 	"strconv"
 )
 
@@ -21,42 +22,35 @@ func main() {
 	for i, s := range strInts {
 		ints[i], _ = strconv.Atoi(s)
 	}
-	fmt.Println(strInts)
+
+	parseNodes(ints)
 }
 
 func parseNodes(def []int) {
 	var nodes []Node
-	var stack []int // queue that points to nodes that are still being progressed (stack)
 
-	state := 0 // 0=new node (header:childCount), 1=header:metaCount, 2=metaEntry
-	for i, val := range def {
-		switch state {
-		case 0:
-			nodes = append(nodes, Node{id: len(nodes), childCount: val})
-			stack = append(stack, len(nodes)-1)
-			parentNode := stack[len(stack)-2]
-			nodes[parentNode].children = append(nodes[parentNode].children, len(nodes)-1)
-			state = 1
-		case 1:
-			activeNode := stack[len(stack)-1]
-			nodes[activeNode].metaCount = val
-			if nodes[activeNode].childCount > 0 {
-				state = 0
-			} else {
-				state = 2
-			}
-		case 2:
-			activeNode := stack[len(stack)-1]
-			nodes[activeNode].meta = append(nodes[activeNode].meta, val)
-			if len(nodes[activeNode].meta) == nodes[activeNode].metaCount {
-				// all meta values read; pop node from open stack
-				stack = stack[:len(stack)-1]
-				nextNode := stack[len(stack)-1]
-				if nodes[nextNode].childCount == len(nodes[nextNode].children) {
-					// all children handled
-					
-				}
-			} else 
+	getNode(def, &nodes)
+	score := 0
+	for _, n := range nodes {
+		for _, m := range n.meta {
+			score += m
 		}
 	}
+	fmt.Println("Solution part1:", score)
+}
+
+func getNode(def []int, nodes *[]Node) (Node, int) {
+	n := Node{id: rand.Int(), childCount: def[0], metaCount: def[1]}
+	offset := 2
+	for i := 0; i < n.childCount; i++ {
+		child, additionalOffset := getNode(def[offset:], nodes)
+		n.children = append(n.children, child.id)
+		offset += additionalOffset
+	}
+	for i := 0; i < n.metaCount; i++ {
+		n.meta = append(n.meta, def[offset])
+		offset++
+	}
+	*nodes = append(*nodes, n)
+	return n, offset
 }
