f15d82e444757156282bc15a4e8039e5bdaabacd
anoff <offenhaeuser@gmail.com>
2018-12-14 14:18:54 +0100

    :scissors: refactor: use int array instead of strings

--- day14/go/anoff/src/solution.go
+++ day14/go/anoff/src/solution.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	//input := 170641
+	input := 170641
 
-	board := "37"
-	cook(&board, 0, 1)
-
-	fmt.Printf("Solution part1: %d\n", 2)
+	board := []int{3, 7}
+	r1 := 0
+	r2 := 1
+	for {
+		board, r1, r2 = cook(board, r1, r2)
+		if len(board) > input+10 {
+			break
+		}
+	}
+	b10 := board[input:input+10]
+	fmt.Printf("Solution part1: %d\n", b10)
 }
 
 // cook recipe 1+2 and update the board
 // 	returns index of two new recipes
-func cook(board *string, recipe1 int, recipe2 int) (int, int) {
-	s := strings.Split(*board, "")
-	s1, _ := strconv.Atoi(s[recipe1])
-	s2, _ := strconv.Atoi(s[recipe2])
-	result := strconv.Itoa(s1+s2)
-	*board += result
-	maxRecipe := len(*board)
+func cook(board []int, recipe1 int, recipe2 int) ([]int, int, int) {
+	s1 := board[recipe1]
+	s2 := board[recipe2]
+	result := s1+s2
+	if result > 9 {
+		board = append(board, result/10)
+	}
+	board = append(board, (result%10))
+	maxRecipe := len(board)
 	r1 := (recipe1+s1+1) % maxRecipe
 	r2 := (recipe2+s2+1) % maxRecipe
 
-	return r1, r2
+	return board, r1, r2
 }
