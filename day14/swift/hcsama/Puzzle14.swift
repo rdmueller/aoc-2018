4a9ec0fbea6485736066747b028b164c15785288
hcsama <hc@aplaypowered.com>
2018-12-14 00:07:01 +0000

    Day13 + prep day14

---
+++ day14/swift/hcsama/Puzzle14.swift
@@ -0,0 +1,19 @@
+//
+//  Puzzle14.swift
+//  AdventOfCode2018
+//
+//  Created by Hans-Christian Fuchs on 14.12.18.
+//  Copyright © 2018 Hans-Christian Fuchs. All rights reserved.
+//
+
+import Foundation
+
+
+func Puzzle14()
+{
+    let file = try! String(contentsOfFile: "/Users/hch/Documents/XCode/AdventOfCode2018/AdventOfCode2018/day14.txt")
+    var lines = file.components(separatedBy: "\n").dropLast()
+    
+    print("14-01:", "TODO")
+    print("14-02:", "TODO")
+}
