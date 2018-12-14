d93641ada0f789e45b46aff89d7b81214750e4bb
anoff <offenhaeuser@gmail.com>
2018-12-14 14:37:43 +0100

    :page_facing_up: docs: day14

--- day14/go/anoff/src/solution.go
+++ day14/go/anoff/src/solution.go
@@ -18,8 +18,31 @@ func main() {
 	}
 	b10 := board[input:input+10]
 	fmt.Printf("Solution part1: %d\n", b10)
+
+	// part 2
+	targetScore := []int{1,7,0,6,4,1}
+	board = []int{3, 7}
+	r1 = 0
+	r2 = 1
+	for {
+		board, r1, r2 = cook(board, r1, r2)
+		if len(board) > len(targetScore) {
+			scoreFound := true
+			for ix := range targetScore {
+				if board[len(board) - 1 - (len(targetScore) - ix)] != targetScore[ix] {
+					scoreFound = false
+					break
+				}
+			}
+			if scoreFound {
+				break
+			}
+		}
+	}
+	fmt.Printf("Solution for part2: %d\n", len(board) - len(targetScore) - 1)
 }
 
+// tag::cook[]
 // cook recipe 1+2 and update the board
 // 	returns index of two new recipes
 func cook(board []int, recipe1 int, recipe2 int) ([]int, int, int) {
@@ -36,3 +59,4 @@ func cook(board []int, recipe1 int, recipe2 int) ([]int, int, int) {
 
 	return board, r1, r2
 }
+// end::cook[]
