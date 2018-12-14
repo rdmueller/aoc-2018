08ba38f239d34590c7e3fed38165953a25643333
anoff <offenhaeuser@gmail.com>
2018-12-14 15:23:58 +0100

    :star2: feat: network.print()

--- day13/go/anoff/src/solution.go
+++ day13/go/anoff/src/solution.go
@@ -29,6 +29,35 @@ func (n *Network) moveCarts() *Network {
 	return n
 }
 
+func (n *Network) hasCart(x int, y int) (bool, Cart) {
+	for _, cart := range n.carts {
+		if cart.x == x && cart.y == y {
+			return true, cart
+		}
+	}
+	return false, Cart{}
+}
+func (n *Network) print() *Network {
+	for y, line := range n.lines {
+		for x, c := range line {
+			if hasCart, cart := n.hasCart(x, y); hasCart {
+				if cart.dx == 1 {
+					fmt.Print(">")
+				} else if cart.dx == -1 {
+					fmt.Print("<")
+				} else if cart.dy == 1 {
+					fmt.Print("^")
+				} else if cart.dy == -1 {
+					fmt.Print("v")
+				}
+			} else {
+				fmt.Print(string(c))
+			}
+		}
+		fmt.Println("")
+	}
+	return n
+}
 // return 
 func parseInput(input []string) Network {
 	var net Network
@@ -59,9 +88,7 @@ func parseInput(input []string) Network {
 func main() {
 	input := readInput("../test.txt")
 	net := parseInput(input)
-	for _, line := range net.lines {
-		fmt.Println(line)
-	}
+	net.print()
 	fmt.Println(len(net.carts))
 }
 
