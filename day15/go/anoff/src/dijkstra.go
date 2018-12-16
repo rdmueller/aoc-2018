a275298fd5bae2e1f89f14726a06dfb93510ff97
anoff <offenhaeuser@gmail.com>
2018-12-16 01:22:08 +0100

    :star2: feat: bunch of tests & dijkstra \o/

---
+++ day15/go/anoff/src/dijkstra.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type node struct {
+	pos Position
+	prev *node
+	distance int
+}
+func newNode(pos Position) node {
+	return node{pos:pos, distance:math.MaxInt32}
+}
+type grid struct {
+	nodes []*node
+}
+func (g *grid) addNode(n *node) *grid {
+	g.nodes = append(g.nodes, n)
+	return g
+}
+
+func (g *grid) getShortest() *node {
+	shortestN := g.nodes[0]
+	shortestDistance := g.nodes[0].distance
+	for _, n := range g.nodes {
+		if n.distance < shortestDistance {
+			shortestDistance = n.distance
+			shortestN = n
+		}
+	}
+	return shortestN
+}
+
+func (g *grid) remove(d *node) *grid {
+	for i, n := range g.nodes {
+		if n.pos.IsEqual(d.pos) {
+			g.nodes = append(g.nodes[:i], g.nodes[i+1:]...)
+		}
+	}
+	return g
+}
+
+func (g *grid) getNeighbors(d *node) []*node {
+	var neighbors []*node
+	for _, n := range g.nodes {
+		d := manhattanDistance(d.pos.x, d.pos.y, n.pos.x, n.pos.y)
+		if d == 1 {
+			neighbors = append(neighbors, n)
+		}
+	}
+	return neighbors
+}
+// a dijkstra implementation that only allows simple 2D movement (non diagonal)
+// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
+func Dijkstra2D(walkable []Position, start Position, dest Position) []Position {
+	var Q grid
+	for _, p := range walkable {
+		n := newNode(p)
+		if p.IsEqual(start) {
+			n.distance = 0
+		}
+		Q.addNode(&n)
+	}
+	
+	for {
+		u := Q.getShortest()
+		Q.remove(u)
+		if u.pos.IsEqual(dest) {
+			fmt.Print("")
+			steps := []Position{u.pos}
+			for p := u.prev; p.prev != nil; p = p.prev {
+				steps = append([]Position{p.pos}, steps...)
+			}
+			return steps
+		}
+		for _, n := range Q.getNeighbors(u) {
+			newDist := u.distance + 1
+			if newDist < n.distance {
+				n.distance = newDist
+				n.prev = u
+			}
+		}
+	}
+}
+
+func manhattanDistance(x1 int, y1 int, x2 int, y2 int) int {
+	var diff int
+	if x1 < x2 {
+		diff += x2-x1
+	} else {
+		diff += x1-x2
+	}
+
+	if y1 < y2 {
+		diff += y2 - y1
+	} else {
+		diff += y1 - y2
+	}
+
+	return diff
+}
---
+++ day15/go/anoff/src/dijkstra_test.go
@@ -0,0 +1,83 @@
+package main
+import (
+	"testing"
+	"fmt"
+)
+
+func TestGrid(t *testing.T) {
+	var g grid
+	tables := []struct{
+		x int
+		y int
+	}{
+		{0, 1},
+		{1, 1},
+		{0, 0},
+		{0, 2},
+		{1, 2},
+	}
+	for _, t := range tables {
+		n := newNode(Position{t.x, t.y})
+		g.addNode(&n)
+	}
+	g.nodes[3].distance = 12
+	shortN := g.getShortest()
+	if shortN.distance != 12 {
+		t.Error("Wrong node with shortest distance returned")
+	}
+	n := newNode(Position{1, 1})
+	ns := g.getNeighbors(&n)
+	if len(ns) != 2 {
+		t.Error("Wrong number of neighbors in grid")
+	}
+	for _, n := range ns {
+		if !n.pos.IsEqual(Position{1, 2}) && !n.pos.IsEqual(Position{0, 1}) {
+			t.Error("Invalid neighbor returned")
+		}
+	}
+}
+
+func TestGridRemove(t *testing.T) {
+	var g grid
+	tables := []struct{
+		x int
+		y int
+	}{
+		{0, 1},
+		{1, 1},
+		{0, 0},
+		{0, 2},
+		{1, 2},
+	}
+	for _, t := range tables {
+		n := newNode(Position{t.x, t.y})
+		g.addNode(&n)
+	}
+	n := newNode(Position{2, 2})
+	g.remove(&n) // should not throw error :shrug:
+	if len(g.nodes) != 5 {
+		t.Error("Node has been removed Oo")
+	}
+	n = newNode(Position{0, 2})
+	g.remove(&n)
+	if len(g.nodes) != 4 {
+		t.Error("Node has not been removed :(")
+	}
+}
+
+func TestDijkstra2D(t *testing.T) {
+	var walkable []Position
+	for x := 0; x < 10; x++ {
+		for y := 0; y < 10; y++ {
+			walkable = append(walkable, Position{x, y})
+		}
+	}
+	path := Dijkstra2D(walkable, Position{0, 0}, Position{1, 3})
+	if len(path) != 4 {
+		t.Error("Wrong path length for unobstructed path")
+	}
+}
+
+func testDummy() {
+	fmt.Println("")
+}
