7ecd5b4adefbf0b6e2199c04d719c1d1416e5c15
anoff <offenhaeuser@gmail.com>
2018-12-18 08:52:37 +0100

    :star2: feat: part1

--- day18/go/anoff/src/solution.go
+++ day18/go/anoff/src/solution.go
@@ -66,22 +66,54 @@ func (a *area) tick() *area {
 	}
 	return a
 }
+func (a *area) countFieldsOfState(state string) int {
+	count := 0
+	for _, f := range a.fields {
+		if f.state == state {
+			count++
+		}
+	}
+	return count
+}
+func (a *area) score() int {
+	return a.countFieldsOfState("|")*a.countFieldsOfState("#")
+}
 func main() {
-	input := readInput("../test.txt")
+	input := readInput("../input.txt")
 	part1(input)
 	part2(input)
 }
 
 func part1(input []string) {
 	a := NewAreaFromInput(input)
-	a.print()
-	fmt.Println("")
-	a.tick().print()
-	fmt.Println("Solution for part1:", 0)
+	for minute := 0; minute < 10; minute++ {
+		a.tick()
+	}
+	fmt.Println("Solution for part1:", a.score())
 }
 
 func part2(input []string) {
-	
+	a := NewAreaFromInput(input)
+	deltaScoreK1 := 0
+	scoreK1 := 0
+	minute := 0
+	for i := 0; i < 10000; i++ {
+		a.tick()
+		score := a.score()
+		deltaScore := score - scoreK1
+		if deltaScore == deltaScoreK1 {
+			fmt.Println("Found recurring delta", minute, deltaScore)
+		}
+		if score == scoreK1 {
+			fmt.Println("Found recurring score", minute, score)
+		}
+		if minute > 1000 {
+			fmt.Println(score, scoreK1)
+		}
+		deltaScoreK1 = deltaScore
+		scoreK1 = score
+		minute++
+	}
 	fmt.Println("Solution for part2:", 0)
 }
 
