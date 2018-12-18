6833ccb7d6420ae1e2389c41bcfeda0375d91b27
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-18 07:39:24 +0100

    Day 18 part 2 solved.

--- day18/java/highsource/src/main/java/Solution.java
+++ day18/java/highsource/src/main/java/Solution.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
@@ -16,22 +18,47 @@ public class Solution {
 			}
 
 			Board initialBoard = builder.build();
-			System.out.println(initialBoard);
+//			System.out.println(initialBoard);
 
 			// Part 1
 			{
 				Board board = initialBoard;
 
-				System.out.println(board);
-
 				for (int step = 0; step < 10; step++) {
 					board = board.next();
 
-					System.out.println("=[" + step + "]=========================");
-					System.out.println(board.toString());
+//					System.out.println("=[" + step + "]=========================");
+//					System.out.println(board.toString());
 				}
 
-				System.out.println("Resource value after 10 steps:" + board.resourceValue());
+				System.out.println("Resource value after 10 steps: " + board.resourceValue());
+			}
+
+			// Part 2
+			{
+				Board board = initialBoard;
+
+				Map<Board, Integer> boardOnStep = new HashMap<>();
+
+				for (int step = 0; step < 1000000000; step++) {
+					board = board.next();
+					Integer oldStep = boardOnStep.put(board, step);
+					if (oldStep != null) {
+						final int period = step - oldStep;
+
+//						System.out.println("Step [" + step + "]. The same board was seen on step [" + oldStep+ "]. Period is [" + period + "].");
+
+						final int start = oldStep % period;
+						if ((step % period) != start) {
+							throw new AssertionError();
+						}
+
+						if ((999999999 % period) == start) {
+							System.out.println("Resource value after 1000000000 steps: " + board.resourceValue());
+							break;
+						}
+					}
+				}
 			}
 		}
 	}
