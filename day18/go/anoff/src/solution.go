dbbf0cea70dd81ddc35beadd27787df7daae30bb
anoff <offenhaeuser@gmail.com>
2018-12-18 01:44:55 +0100

    :star2: feat: setup 18

---
+++ day18/go/anoff/src/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+type coord struct {
+	x, y int
+}
+
+func main() {
+	input := readInput("../test.txt")
+	part1(input)
+	part2(input)
+}
+
+func part1(input []string) {
+	
+	fmt.Println("Solution for part1:", 0)
+}
+
+func part2(input []string) {
+	
+	fmt.Println("Solution for part2:", 0)
+}
