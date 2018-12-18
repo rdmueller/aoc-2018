a922ed3230d3598ec06c13e76477163e36d7e4d1
matason <chrispmaiden@gmail.com>
2018-12-18 20:41:35 +0000

    Fixed part 1 indentation.

--- day01/lisp/matason/solution.lisp
+++ day01/lisp/matason/solution.lisp
@@ -3,6 +3,6 @@
 ;;; Part 1
 (with-open-file (input "input.txt")
   (loop for change = (read-line input nil)
-        while change
-        sum (parse-integer change) into frequency
-        finally (return (format t "~@d~%" frequency))))
+     while change
+     sum (parse-integer change) into frequency
+     finally (return (format t "~@d~%" frequency))))
