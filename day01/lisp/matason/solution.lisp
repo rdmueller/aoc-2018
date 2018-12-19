93f11de9f4a423a203e576756f2474905d82d3d8
matason <chrispmaiden@gmail.com>
2018-12-19 11:27:43 +0000

    Day 1, part 2.
    I won't be saving Christmas at this rate... I've added a solution for
    day 1, part 2 (it's very slow) and generally tidied up.

--- day01/lisp/matason/solution.lisp
+++ day01/lisp/matason/solution.lisp
@@ -1,8 +1,29 @@
 ;;;; Chronal Calibration
 
 ;;; Part 1
-(with-open-file (input "input.txt")
-  (loop for change = (read-line input nil)
-     while change
-     sum (parse-integer change) into frequency
-     finally (return (format t "~@d~%" frequency))))
+(defun part1 ()
+  (reduce '+ (funcall 'input-to-list)))
+
+;;; Part 2 - I'm doing something particularly inefficient here, part2 takes a
+;;; minute or more to execute.
+(defun part2 ()
+  (defparameter adjustments (funcall 'input-to-circular-list))
+  (loop for adjustment in adjustments with total = 0 with seen = '() do
+       (setq total (+ total adjustment))
+       (if (find total seen) (return total))
+       (push total seen)))
+
+;;; Get a list of the input data.
+(defun input-to-list ()
+  (with-open-file (input "input.txt")
+    (loop for adjustments = (read-line input nil)
+       while adjustments
+       collect (parse-integer adjustments))))
+
+;;; Get a circular list of the input data.
+(defun input-to-circular-list()
+  (defparameter adjustments (funcall 'input-to-list))
+  (setf (cdr (last adjustments)) adjustments))
+
+(format t "~@d~%" (funcall 'part1))
+(format t "~@d~%" (funcall 'part2))
