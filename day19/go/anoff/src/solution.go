1be954589f16e4ab2a35e2ad03f93afa0e4b31d5
anoff <offenhaeuser@gmail.com>
2018-12-19 16:42:47 +0100

    :star2: feat: assembling manually..

--- day19/go/anoff/src/solution.go
+++ day19/go/anoff/src/solution.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 )
 
-var DEBUG = false
+var DEBUG = true
 
 func main() {
 	input := readInput("../input.txt")
 	p := NewProgram(input)
 	p.print()
-	part1(&p)
+	//part1(&p)
+	p = NewProgram(input)
+	part2(&p)
 }
 
 func part1(p *program) {
@@ -18,6 +20,17 @@ func part1(p *program) {
 		programEnded := p.step(DEBUG)
 		if programEnded {
 			fmt.Println("Solution for part 1:", p.registers[0])
+			break
+		}
+	}
+}
+
+func part2(p *program) {
+	p.registers[0] = 1
+	for i := 0; i < 20; i++ {
+		programEnded := p.step(DEBUG)
+		if programEnded {
+			fmt.Println("Solution for part 2:", p.registers[0])
 			break
 		}
 	}
