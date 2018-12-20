ad7c6c15370d4092759a318032b465f99e16fd5d
anoff <offenhaeuser@gmail.com>
2018-12-20 13:02:25 +0100

    :star2: feat: test entire room

--- day20/go/anoff/src/solution.go
+++ day20/go/anoff/src/solution.go
@@ -11,16 +11,15 @@ func main() {
 	fmt.Println("Solution for part 1:", 0)
 }
 
-func part1(input string) {
+func part1(input string) *Room {
 	paths := expandPattern(input)
 	room := NewRoom()
 	for _, p := range paths {
-		fmt.Println(p)
 		path := NewPath(p, &room)
 		path.walk()
 	}
 	room.fillWalls()
-	room.print()
+	return &room
 }
 func animate(path *Path) {
 	for {
---
+++ day20/go/anoff/src/solution_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestPathWalked(t *testing.T) {
+	m := map[string]string{
+		"../test0.txt": "../test0-out.txt",
+		"../test1.txt": "../test1-out.txt",
+		"../test2.txt": "../test2-out.txt",
+	}
+	for in, out := range m {
+		input := strings.Join(readInput(in), "")
+		exp := readInput(out)
+		r := part1(input)
+		for y, line := range exp {
+			for x, _ := range line {
+				if r.rows[y][x] != exp[y][x] {
+					t.Errorf("Wrong character at (%d,%d) expected %s, found %s", x, y, string(exp[y][x]), string(r.rows[y][x]))
+				}
+			}
+		}
+	}
+}
