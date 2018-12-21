b39227811cdf37d4fd6fd68287a31d27fd6dce32
Tobias Schulte <tobias.schulte@gliderpilot.de>
2018-12-22 00:09:08 +0100

    prepare day 11

---
+++ day11/javascript/tschulte/solution.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const { parseLine } = require("./part1");
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf-8");
+const lines = input.split("\n").filter(line => line);
+
+// tag::part1[]
+
+console.log("Day 05, part 1: ");
+//end::part1[]
+
+// tag::part2[]
+console.log("Day 05, part 2: ");
+// end::part2[]
