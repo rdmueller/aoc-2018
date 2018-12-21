36d859b7328ec21bde76e7db8cfffd22f07c7697
anoff <offenhaeuser@gmail.com>
2018-12-21 11:26:19 +0100

    :star2: feat: part1+2

--- day21/go/anoff/src/solution.go
+++ day21/go/anoff/src/solution.go
@@ -4,27 +4,51 @@ import (
 	"fmt"
 )
 
-var DEBUG = true
+var DEBUG = false
 
 func main() {
 	input := readInput("../input.txt")
 	p := NewProgram(input)
 	p.print()
 	part1(&p)
-	//part2(&p)
+	p = NewProgram(input)
+	part2(&p)
 }
 
 func part1(p *program) {
-	
+	for {
+		programEnded := p.step(DEBUG)
+		if p.registers[p.pointerRegister] == 28 {
+			fmt.Println("Solution for part 1:", p.registers[3])
+			break
+		}
+		if programEnded {
+			break
+		}
+	}
 }
 
 func part2(p *program) {
-	E := 10550400 + 875
-	A := 0
-	for B := 1; B <= E; B++ {
-		if E % B == 0 {
-			A += B
+	seen := make(map[int]int)
+	prev := 0
+	i := 0
+	for {
+		programEnded := p.step(DEBUG)
+		if p.registers[p.pointerRegister] == 28 {
+			d := p.registers[3]
+			if _, has := seen[d]; has {
+				fmt.Println("Solution for part 2:", prev, ", (after seeing", d, "twice)")
+				break
+			}
+			if len(seen) % 500 == 0 {
+				fmt.Println("seen", len(seen), "results")
+			}
+			seen[d] = i
+			prev = d
 		}
+		if programEnded {
+			break
+		}
+		i++
 	}
-	fmt.Println("Solution for part 2:", A)
 }
