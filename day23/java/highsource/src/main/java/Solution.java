3d5e3c6c086840985094e5257afafa36828bfbf9
Alexey Valikov <aleksei.valikov@gmail.com>
2018-12-23 17:21:24 +0100

    Day 23 working on part 2.

--- day23/java/highsource/src/main/java/Solution.java
+++ day23/java/highsource/src/main/java/Solution.java
@@ -25,12 +25,30 @@ public class Solution {
 
 			// Part 1
 			{
-				final Nanobot nanobotWithLargestSignalRadius = nanobots.stream().max(Comparator.comparingInt(Nanobot::getR))
-						.orElseThrow(IllegalStateException::new);
+				final Nanobot nanobotWithLargestSignalRadius = nanobots.stream()
+						.max(Comparator.comparingInt(Nanobot::getR)).orElseThrow(IllegalStateException::new);
 
 				final long nanobotsInRange = nanobots.stream().filter(nanobotWithLargestSignalRadius::isInRange)
 						.count();
 				System.out.println("Number of nanobots in range of the toughest nanobot is: " + nanobotsInRange);
+			}
+
+			// Part 2
+			{
+				
+				int minX = nanobots.stream().mapToInt(Nanobot::getX).min().orElseThrow(IllegalStateException::new);
+				int minY = nanobots.stream().mapToInt(Nanobot::getY).min().orElseThrow(IllegalStateException::new);
+				int minZ = nanobots.stream().mapToInt(Nanobot::getZ).min().orElseThrow(IllegalStateException::new);
+				int maxX = nanobots.stream().mapToInt(Nanobot::getX).max().orElseThrow(IllegalStateException::new);
+				int maxY = nanobots.stream().mapToInt(Nanobot::getY).max().orElseThrow(IllegalStateException::new);
+				int maxZ = nanobots.stream().mapToInt(Nanobot::getZ).max().orElseThrow(IllegalStateException::new);
+				
+				System.out.println(minX);
+				System.out.println(minY);
+				System.out.println(minZ);
+				System.out.println(maxX);
+				System.out.println(maxY);
+				System.out.println(maxZ);
 
 			}
 		}
