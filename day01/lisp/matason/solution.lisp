ff960f2da971c3b3fd6678e22d8929e7133358de
matason <chrispmaiden@gmail.com>
2018-12-23 09:35:19 +0000

    Remove funcall.

--- day01/lisp/matason/solution.lisp
+++ day01/lisp/matason/solution.lisp
@@ -2,12 +2,12 @@
 
 ;;; Part 1
 (defun part1 ()
-  (reduce '+ (funcall 'input-to-list)))
+  (reduce '+ (input-to-list)))
 
 ;;; Part 2 - I'm doing something particularly inefficient here, part2 takes a
 ;;; minute or more to execute.
 (defun part2 ()
-  (defparameter adjustments (funcall 'input-to-circular-list))
+  (defparameter adjustments (input-to-circular-list))
   (loop for adjustment in adjustments with total = 0 with seen = '() do
        (setq total (+ total adjustment))
        (if (find total seen) (return total))
@@ -22,8 +22,8 @@
 
 ;;; Get a circular list of the input data.
 (defun input-to-circular-list()
-  (defparameter adjustments (funcall 'input-to-list))
+  (defparameter adjustments (input-to-list))
   (setf (cdr (last adjustments)) adjustments))
 
-(format t "~@d~%" (funcall 'part1))
-(format t "~@d~%" (funcall 'part2))
+(format t "~@d~%" (part1))
+(format t "~@d~%" (part2))
