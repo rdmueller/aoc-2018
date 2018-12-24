539b5c072edeabf14be827fa2e53891c68b9eb27
anoff <offenhaeuser@gmail.com>
2018-12-24 11:57:32 +0100

    :star2: feat: calc potential damage

--- day24/go/anoff/src/group.go
+++ day24/go/anoff/src/group.go
@@ -17,3 +17,18 @@ func NewGroup(u Unit, units int) Group {
 	g := Group{u, units}
 	return g
 }
+
+func (g *Group) damagePotential(t *Group) int {
+	d := g.getPower()
+	for _, immune := range t.immunities {
+		if g.attackType == immune {
+			d = 0
+		}
+	}
+	for _, weak := range t.weaknesses {
+		if g.attackType == weak {
+			d *= 2
+		}
+	}
+	return d
+}
